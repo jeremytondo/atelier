febfd960dd6250bdf895a86a2c1adc1f60f76175
jeremytondo <jeremytondo@gmail.com>
2024-12-05 03:27:31 +0000

    Added starter config files

---
+++ config/.config/zsh/aliases.zsh
@@ -0,0 +1,8 @@
+# File system
+alias ls='eza -lh --group-directories-first --icons'
+alias lsa='ls -a'
+alias lt='eza --tree --level=2 --long --icons --git'
+alias lta='lt -a'
+alias ff="fzf --preview 'batcat --style=numbers --color=always {}'"
+alias fd='fdfind'
+alias cd='z'
