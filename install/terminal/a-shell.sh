f8ac82aa566e8e3b3419c85d8e6838791317853f
jeremytondo <jeremytondo@gmail.com>
2024-12-11 12:46:45 +0000

    Additional terminal apps

---
+++ install/terminal/a-shell.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "This is a test message"
+# Install ZSH
+# sudo apt install zsh -y
+
+# Make ZSH the default shell
+# chsh -s $(which zsh)
+
+# Setup zsh configs.
+# [ -f ~/.zshrc ] && mv ~/.zshrc ~/.zshrc.bak
+# cp $SCRIPT_DIR/config/.zshrc ~/.zshrc
+
+# Load the PATH for use later in the installers
+# source ~/.local/share/omakub/defaults/bash/shell
---
+++ install/terminal/general.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# A general default set of apps that make working in the terninal easier.
+sudo apt install -y fzf ripgrep bat eza zoxide plocate btop apache2-utils fd-find tldr
