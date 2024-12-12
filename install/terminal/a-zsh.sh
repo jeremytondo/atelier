8889ed831e744d5ccfe96525dea47c0f2a499f6a
jeremytondo <jeremytondo@gmail.com>
2024-12-12 22:03:30 +0000

    Fixing zsh install login issue.

--- install/terminal/a-zsh.sh
+++ install/terminal/a-zsh.sh
@@ -4,7 +4,7 @@
 sudo apt install zsh -y
 
 # Make ZSH the default shell
-chsh -s $(which zsh)
+sudo chsh -s $(which zsh) $USER
 
 # Load the PATH for use later in the installers
 # source ~/.local/share/omakub/defaults/bash/shell
