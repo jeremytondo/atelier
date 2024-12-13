ec17dbb564b825f9af0fbd13f72fb9b5b12b6d37
Jeremy <jmargari@gmail.com>
2024-12-13 16:55:56 +0000

    Backup bash configs

--- install/terminal/a-zsh.sh
+++ install/terminal/a-zsh.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Backup existing bash config.
+mkdir ~/.bash.bak
+mv ~/.bashrc ~/.bash.bak
+mv ~/.profile ~/.bash.bak
+mv ~/.bash_logout ~/.bash.bak
+mv ~/.bash_history ~/.bash.bak
+
 # Install ZSH
 sudo apt install zsh -y
 
