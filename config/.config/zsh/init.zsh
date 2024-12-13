96a98fe75a8b1a1b22cb1f81e77ae3d15619c2a3
Jeremy <jmargari@gmail.com>
2024-12-13 16:56:30 +0000

    Cleanup zsh config

--- config/.config/zsh/init.zsh
+++ config/.config/zsh/init.zsh
@@ -1,3 +1,15 @@
+# set PATH so it includes user's private bin if it exists
+if [ -d "$HOME/bin" ] ; then
+    PATH="$HOME/bin:$PATH"
+fi
+
+# set PATH so it includes user's private bin if it exists
+if [ -d "$HOME/.local/bin" ] ; then
+    PATH="$HOME/.local/bin:$PATH"
+fi
+
+export PATH=$PATH:/usr/local/go/bin:$HOME/.local/share/atelier/bin
+
 if command -v zoxide &> /dev/null; then
   eval "$(zoxide init zsh)"
 fi
@@ -6,5 +18,3 @@ if command -v fzf &> /dev/null; then
   source /usr/share/doc/fzf/examples/completion.zsh
   source /usr/share/doc/fzf/examples/key-bindings.zsh
 fi
-
-export PATH=$PATH:/usr/local/go/bin:$HOME/.local/share/atelier/bin
--- config/.zshrc
+++ config/.zshrc
@@ -1,4 +1,3 @@
-# This is a test .zshrc file.
 source ~/.config/zsh/init.zsh
 source ~/.config/zsh/aliases.zsh
 source ~/.config/zsh/prompt.zsh
