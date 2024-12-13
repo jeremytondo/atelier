0b497d608845f14f8f9d4fbfdf019d15ca522949
Jeremy <jmargari@gmail.com>
2024-12-13 13:19:33 -0600

    Added git branch to prompt

--- config/.config/zsh/prompt.zsh
+++ config/.config/zsh/prompt.zsh
@@ -2,6 +2,15 @@
 force_color_prompt=yes
 color_prompt=yes
 
+# Load version control information
+autoload -Uz vcs_info
+precmd() { vcs_info }
+
+# Format the vcs_info_msg_0_ variable
+zstyle ':vcs_info:git:*' formats ' %b'
+
 # Simple prompt with path in the window/pane title and caret for typing line
+setopt PROMPT_SUBST
 PS1=$'\uf0a9 '
+RPROMPT=\$vcs_info_msg_0_
 # PS1="\[\e]0;\w\a\]$PS1"
