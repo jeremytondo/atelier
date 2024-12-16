114f4123c4734479cc70dbba870bec0159ea3aab
Jeremy <jmargari@gmail.com>
2024-12-16 16:04:00 -0600

    Project management and creation

--- bin/scripts/dev.sh
+++ bin/scripts/dev.sh
@@ -1,23 +1,55 @@
-# Create/attach to a tmux session set up for editing the Atelier app.
+PROJECTS_FOLDER=$HOME/projects
 
 dev() {
-  session_name="atelier"
+  project_name=$1
+  project_folder=$PROJECTS_FOLDER/$project_name
+
+  # Check for running tmux sessions
+  sessions=$(tmux list-sessions 2>/dev/null)
+
+  if [[ -n "$sessions" ]]; then
+    tmux a
+    return 0
+  else
+    echo "There are no open projects."
+    return 0
+  fi
+
+  if [[ "$project_name" == "atelier" ]]; then
+    open_project atelier
+    return 0
+  fi
+
+  if [ -d "$project_folder" ]; then
+    echo "The folder  exists."
+  else
+    gum confirm "The project does not exist. Would you like to create it?" && create_project $project_name
+  fi
+}
+
+open_project() {
+  project_name=$1
 
   # Attempt to attach to the session, ignoring errors
-  tmux attach-session -t $session_name 2>/dev/null
+  tmux attach-session -t $project_name 2>/dev/null
 
   # If the attachment fails, create a new session
   if [ $? -ne 0 ]; then
     # Create the session and open Neovim in the first window
-    tmux new-session -s $session_name -d -c ~/.local/share/atelier/ 'nvim .'
+    tmux new-session -s $project_name -d -c ~/.local/share/atelier/ 'nvim .'
 
     # Create a new window
-    tmux new-window -t $session_name:2
+    tmux new-window -t $project_name:2
 
     # Switch back the first window
-    tmux select-window -t $session_name:1
+    tmux select-window -t $project_name:1
 
     # Attach to the session.
-    tmux attach-session -t $session_name
+    tmux attach-session -t $project_name
   fi
+}
+
+create_project() {
+  project_name=$1
+  mkdir -p $PROJECTS_FOLDER/$project_name
 }
