526161c02b4c9b8d697fe3c017cd62938d166445
Jeremy <jmargari@gmail.com>
2024-12-16 05:52:57 -0600

    zsh autocompletions

--- config/.config/zsh/init.zsh
+++ config/.config/zsh/init.zsh
@@ -18,3 +18,7 @@ if command -v fzf &> /dev/null; then
   source /usr/share/doc/fzf/examples/completion.zsh
   source /usr/share/doc/fzf/examples/key-bindings.zsh
 fi
+
+# ZSH auto suggestions.
+source /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+
