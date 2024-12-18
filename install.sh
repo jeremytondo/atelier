ddc34f6a5e9db82f46cf0e91d46fc46760ea38e4
Jeremy <jmargari@gmail.com>
2024-12-18 18:19:49 +0000

    Testing MacOS Detection

--- install.sh
+++ install.sh
@@ -7,28 +7,38 @@ set -e
 # SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 SCRIPT_DIR=$HOME/.local/share/atelier
 
+# OS type: Linux or Darwin (MacOS)
+OS="$(uname -s)"
+
 # Includes some shared scripts needed for the install process.
 source $SCRIPT_DIR/bin/scripts/shared.sh
 
-# Ensure everything is update before beginning installation.
-sudo apt update -y
-sudo apt upgrade -y
+if [[ "$OS" == "Linux" ]]; then
+  echo "Linux"
+  # Ensure everything is update before beginning installation.
+  # sudo apt update -y
+  # sudo apt upgrade -y
+fi
+
+if [[ "$OS" == "Darwin" ]]; then
+  echo "MacOS"
+fi
 
 # Installs required apps needed for the rest of the install process.
-for app in $SCRIPT_DIR/install/required/*.sh; do source $app; done
+# for app in $SCRIPT_DIR/install/required/*.sh; do source $app; done
 
 # Install Linux terminal apps.
-for app in $SCRIPT_DIR/install/terminal/*.sh; do source $app; done
+# for app in $SCRIPT_DIR/install/terminal/*.sh; do source $app; done
 
 # INSTALL CONFIGS
 
 # Backup existing configs.
-[ -f "~/.zshrc" ] && mv ~/.zshrc ~/.zshrc.bak
-[ -d "~/.config" ] && mv ~/.bashrc ~/.bashrc.bak
+# [ -f "~/.zshrc" ] && mv ~/.zshrc ~/.zshrc.bak
+# [ -d "~/.config" ] && mv ~/.bashrc ~/.bashrc.bak
 
 # Use GNU Stow to to link configs to home directory.
-stow . -d $SCRIPT_DIR/config -t ~/
+# stow . -d $SCRIPT_DIR/config -t ~/
 
 # Install global programmig languages witn mise.
-mise use --global node@lts
-mise use --global go@latest
+# mise use --global node@lts
+# mise use --global go@latest
