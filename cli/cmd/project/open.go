b52d4f8d045b4070fa221fb5097ef18bd65c0162
jeremytondo <jmargari@gmail.com>
2025-01-02 22:02:31 -0600

    Working tmux functionality

--- cli/cmd/project/open.go
+++ cli/cmd/project/open.go
@@ -2,11 +2,13 @@ package project
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
 
 	"github.com/spf13/cobra"
 )
 
-// projectCmd represents the project command
 var openCmd = &cobra.Command{
 	Use:   "open",
 	Short: "A brief description of your command",
@@ -17,20 +19,51 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("project open called")
+		homeDir, _ := os.UserHomeDir()
+		projectsDir := "projects"
+		projectName, _ := cmd.Flags().GetString("name")
+		projectPath := filepath.Join(homeDir, projectsDir, projectName)
+
+		fmt.Println("project open called with name:", projectPath)
+
+		// Check if the project exists.
+		_, err := os.Stat(projectPath)
+		if os.IsNotExist(err) {
+			// TODO: Eventually ask if user wants to create a project.
+			fmt.Println("Project does not exist.")
+			return
+		}
+
+		openProject(projectName, projectPath)
 	},
 }
 
 func init() {
-	// rootCmd.AddCommand(projectCmd)
+	openCmd.Flags().StringP("name", "n", "", "Name of the project to open")
+}
 
-	// Here you will define your flags and configuration settings.
+func openProject(projectName string, projectPath string) error {
+	// Helper function to run tmux commands
+	runTmuxCmd := func(args ...string) error {
+		cmd := exec.Command("tmux", args...)
+		cmd.Stdin = os.Stdin
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		return cmd.Run()
+	}
 
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// projectCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// Attempt to attach to existing project session
+	err := runTmuxCmd("attach-session", "-t", projectName)
+	if err == nil {
+		return nil
+	}
 
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// projectCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// If a session does not exist, create a new one.
+	args := []string{"new-session", "-s", projectName, "-d", "-c", projectPath, "nvim", "."}
+	if err := runTmuxCmd(args...); err != nil {
+		return fmt.Errorf("failed to create new session: %w", err)
+	}
+
+	// Attach to the newly created session
+	return runTmuxCmd("attach-session", "-t", projectName)
 }
