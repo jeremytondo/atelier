883bce8f377590b837cc84e95aadec98c696b6a1
jeremytondo <jmargari@gmail.com>
2025-01-05 19:57:19 -0600

    Added multiplatform support

---
+++ cli/build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# App name
+APP_NAME="atelier"
+
+# Output directory
+OUTPUT_DIR="bin"
+
+# Platforms to build for
+PLATFORMS=("darwin/amd64" "darwin/arm64" "linux/amd64")
+
+# Clean up previous builds
+rm -rf $OUTPUT_DIR
+mkdir -p $OUTPUT_DIR
+
+# Loop through platforms and build
+for PLATFORM in "${PLATFORMS[@]}"; do
+  IFS="/" read -r GOOS GOARCH <<<"$PLATFORM"
+
+  OUTPUT_PATH="$OUTPUT_DIR/$APP_NAME-$GOOS-$GOARCH"
+
+  echo "Building for $GOOS/$GOARCH..."
+  env GOOS=$GOOS GOARCH=$GOARCH go build -o $OUTPUT_PATH ./cmd
+  if [ $? -ne 0 ]; then
+    echo "Failed to build for $GOOS/$GOARCH!"
+    exit 1
+  fi
+done
+
+echo "Builds completed! Check the '$OUTPUT_DIR' directory."
--- install/apps/cli.sh
+++ install/apps/cli.sh
@@ -1,10 +1,38 @@
 # Add the Atelier cli to the local bin.
 # Uses a simlink to make updates easy.
-source="$HOME/.local/share/atelier/cli/bin/atelier"
-symlink="$HOME/.local/bin"
 
-# Make sure the directory exists
-mkdir -p "$symlink"
+# Supported platforms
+SUPPORTED_PLATFORMS=("darwin/amd64" "darwin/arm64" "linux/amd64")
 
-# If the symlink does not exist, create it.
-[[ ! -L "$symlink" ]] && ln -s "$source" "$symlink/atelier"
+# Get OS and architecture
+OS=$(uname -s | tr '[:upper:]' '[:lower:]') # Converts to lowercase
+ARCH=$(uname -m)
+
+# Normalize architecture
+case $ARCH in
+x86_64)
+  ARCH="amd64"
+  ;;
+arm64 | aarch64)
+  ARCH="arm64"
+  ;;
+esac
+
+# Current platform
+CURRENT_PLATFORM="$OS/$ARCH"
+
+# Check if the current platform is supported
+if [[ " ${SUPPORTED_PLATFORMS[*]} " =~ " $CURRENT_PLATFORM " ]]; then
+  source="$HOME/.local/share/atelier/cli/bin/atelier-$OS-$ARCH"
+  symlink="$HOME/.local/bin"
+
+  # Make sure the directory exists
+  mkdir -p "$symlink"
+
+  # If the symlink does not exist, create it.
+  [[ ! -L "$symlink" ]] && ln -s "$source" "$symlink/atelier"
+  # exit 0
+else
+  echo "Current platform ($CURRENT_PLATFORM) is not supported."
+  # exit 1
+fi
