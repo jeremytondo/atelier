a8aa43e6545bb669935175850b0b5de9d2423de3
jeremytondo <jmargari@gmail.com>
2025-01-09 14:37:08 +0000

    Added shpool session prompt

--- config/.config/zsh/prompt.zsh
+++ config/.config/zsh/prompt.zsh
@@ -14,8 +14,11 @@ setopt PROMPT_SUBST
 
 if [[ "$(uname -s)" == "Darwin" ]]; then
   PS1=$'\uf0a9 '
+  RPROMPT="\$vcs_info_msg_0_"
 else
   PS1=$'%{\e[34m%}\uf0a9 %{\e[0m%}'
+  # RPROMPT="\$vcs_info_msg_0_ \uf0a9\$SHPOOL_SESSION_NAME"
+  RPROMPT=$'\$vcs_info_msg_0_${(0)${SHPOOL_SESSION_NAME:+ \uf50c \$SHPOOL_SESSION_NAME}}'
 fi
-RPROMPT=\$vcs_info_msg_0_
+# RPROMPT="\$vcs_info_msg_0_ \$SHPOOL_SESSION_NAME"
 # PS1="\[\e]0;\w\a\]$PS1"
