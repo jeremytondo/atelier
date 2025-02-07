b10ce731f86505d9e9975f3d1109ebafe6b967b5
jeremytondo <jmargari@gmail.com>
2025-02-07 16:09:23 +0000

    Fixed issues with system clipboard

--- config/.config/nvim/lua/config/options.lua
+++ config/.config/nvim/lua/config/options.lua
@@ -3,3 +3,17 @@
 -- Add any additional options here
 
 vim.opt.colorcolumn = "80" -- Set the line length to 80 characters
+-- vim.opt.clipboard = "unnamed"
+
+-- Force OSC 52 so that copy to system clipboard works even inside an shpool session.
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+  },
+}
