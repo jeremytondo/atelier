6037227537b2f905e09e82a138299c827a42f10a
jeremytondo <jmargari@gmail.com>
2025-02-11 20:24:47 +0000

    Cleanup

--- config/.config/nvim/lua/config/options.lua
+++ config/.config/nvim/lua/config/options.lua
@@ -4,17 +4,17 @@
 
 vim.opt.colorcolumn = "80" -- Set the line length to 80 characters
 
+-- If running locally and not in an ssh session, use the unnamedplus clipboard.
+if vim.fn.empty(vim.env.SSH_TTY) == 1 then -- Running locally (no SSH_TTY)
+  vim.opt.clipboard = "unnamedplus"
+end
+
 -- The following is some attemts to get the clipboard working well via ssh
 -- and when an shpool session is running. Turns out most of this is not needed.
 -- Looks like this was an issue with the SSH_TTY environment variable getting
 -- passed through to the shpool session. I added that to the shpool config
 -- and I think things are working now. Leaving this all here for now though in
 -- case I need to go back to it.
-
-if vim.fn.empty(vim.env.SSH_TTY) == 1 then -- Running locally (no SSH_TTY)
-  vim.opt.clipboard = "unnamedplus"
-end
-
 -- vim.opt.clipboard = "unnamed"
 -- vim.opt.clipboard = ""
 
