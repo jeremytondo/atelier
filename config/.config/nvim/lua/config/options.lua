4d1c168aaec2327e39ba757cd7c8f7bc86f9ae22
jeremytondo <jmargari@gmail.com>
2025-02-11 20:09:29 +0000

    Fixing copy and paste in shpool issues

--- config/.config/nvim/lua/config/options.lua
+++ config/.config/nvim/lua/config/options.lua
@@ -3,6 +3,14 @@
 -- Add any additional options here
 
 vim.opt.colorcolumn = "80" -- Set the line length to 80 characters
+
+-- The following is some attemts to get the clipboard working well via ssh
+-- and when an shpool session is running. Turns out most of this is not needed.
+-- Looks like this was an issue with the SSH_TTY environment variable getting
+-- passed through to the shpool session. I added that to the shpool config
+-- and I think things are working now. Leaving this all here for now though in
+-- case I need to go back to it.
+
 -- vim.opt.clipboard = "unnamed"
 -- vim.opt.clipboard = ""
 
