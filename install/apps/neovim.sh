882031a59f1efcd88e585ace2098607ed57b37f6
jeremytondo <jmargari@gmail.com>
2025-04-24 17:49:45 +0000

    In progress containers

--- install/apps/neovim.sh
+++ install/apps/neovim.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 
-if [[ "$OS" == "Linux" ]]; then
+source "$(dirname "$0")/../shared.sh"
+
+if [[ $(uname -s) == "Linux" ]]; then
   cd /tmp
-  wget -O nvim.tar.gz "https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz"
+  echo "INFO: Downloading Neovim..."
+  wget -O nvim.tar.gz "https://github.com/neovim/neovim/releases/download/v0.11.0/nvim-linux-arm64.tar.gz"
+  
+  echo "INFO: Extracting archive..."
   tar -xf nvim.tar.gz
-  sudo install nvim-linux64/bin/nvim /usr/local/bin/nvim
-  sudo cp -R nvim-linux64/lib /usr/local/
-  sudo cp -R nvim-linux64/share /usr/local/
+  
+  echo "INFO: Installing Neovim..."
+  as_root cp -r nvim-linux64/* /usr/local/
+  
+  echo "INFO: Cleaning up..."
   rm -rf nvim-linux64 nvim.tar.gz
+  
+  echo "INFO: Verifying installation..."
+  if ! command -v nvim >/dev/null 2>&1; then
+    echo "ERROR: nvim not found in PATH"
+    exit 1
+  fi
+  
+  nvim --version || exit 1
   cd -
 else
   brew install neovim
