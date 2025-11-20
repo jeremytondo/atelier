a15333d8640a8b4d0563dac44bc30c2f0b9344eb
Jeremy Margaritondo <jmargari@gmail.com>
2025-11-20 06:50:25 -0600

    Removed old install scripts

--- install/apps-old/gcloud.sh
+++
@@ -1,9 +0,0 @@
-https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-darwin-arm.tar.gz
-
-if [[ "$OS" == "Linux" ]]; then
-  #TODO: Add Linux install.
-else
-  cd /tmp
-  wget -O google-cloud-cli-darwin-arm.tar.gz "https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-darwin-arm.tar.gz"
-  tar -xf google-cloud-cli-darwin-arm.tar.gz
-fi
--- install/apps/zsh-autosuggestions.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source "$(dirname "$0")/install/shared.sh"
-
-if ! [[ -d "/usr/local/share/zsh-autosuggestions" ]]; then
-  as_root mkdir -p /usr/local/share
-  as_root git clone https://github.com/zsh-users/zsh-autosuggestions /usr/local/share/zsh-autosuggestions/
-fi
