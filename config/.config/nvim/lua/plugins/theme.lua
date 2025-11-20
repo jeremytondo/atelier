ba52c5cf6a81c10ac22df4f1a934cbecb91d297e
Jeremy Margaritondo <jeremytondo@Jeremys-Air.localdomain>
2025-11-20 06:26:47 -0600

    Updated Neovim theme

--- config/.config/nvim/lua/plugins/theme.lua
+++ config/.config/nvim/lua/plugins/theme.lua
@@ -3,48 +3,25 @@ return {
     "catppuccin/nvim",
     name = "catppuccin",
     opts = {
-      flavour = "macchiato",
-      integrations = {
-        aerial = true,
-        alpha = true,
-        cmp = true,
-        dashboard = true,
-        flash = true,
-        fzf = true,
-        grug_far = true,
-        gitsigns = true,
-        headlines = true,
-        illuminate = true,
-        indent_blankline = { enabled = true },
-        leap = true,
-        lsp_trouble = true,
-        mason = true,
-        markdown = true,
-        mini = true,
-        native_lsp = {
-          enabled = true,
-          underlines = {
-            errors = { "undercurl" },
-            hints = { "undercurl" },
-            warnings = { "undercurl" },
-            information = { "undercurl" },
-          },
-        },
-        navic = { enabled = true, custom_bg = "lualine" },
-        neotest = true,
-        neotree = true,
-        noice = true,
-        notify = true,
-        semantic_tokens = true,
-        snacks = true,
-        telescope = true,
-        treesitter = true,
-        treesitter_context = true,
-        which_key = true,
-      },
+      flavour = "mocha",
       transparent_background = true,
+      custom_highlights = function(colors)
+        return {
+          -- 1. Force Neo-tree (Explorer) to use the main editor background
+          NeoTreeNormal = { bg = colors.base },
+          NeoTreeNormalNC = { bg = colors.base }, -- Non-focused window
+          -- 2. Force Floating Windows (Snacks Picker / Leader Leader) to use the main editor background
+          NormalFloat = { bg = colors.base },
+          -- FloatBorder = { bg = colors.base, fg = colors.surface0 }, -- Optional: Make the border blends in
+
+          -- 3. Explicitly target Snacks Picker (just to be safe)
+          SnacksPickerNormal = { bg = colors.base },
+          SnacksPickerBorder = { bg = colors.base, fg = colors.surface0 },
+        }
+      end,
     },
   },
+
   -- Configure LazyVim to use catppuccin as the default colorscheme
   {
     "LazyVim/LazyVim",
