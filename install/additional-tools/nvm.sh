e37a1d60485a764851540272952e6628b4e0bdbc
Jeremy Margaritondo <jmargari@gmail.com>
2025-12-14 15:25:55 -0600

    Install initial versoin of node

--- install/additional-tools/nvm.sh
+++ install/additional-tools/nvm.sh
@@ -14,3 +14,13 @@ if [ -d "$NVM_DIR" ]; then
 else
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | PROFILE=/dev/null bash
 fi
+
+# Load nvm to use it in this script
+if [ -s "$NVM_DIR/nvm.sh" ]; then
+  \. "$NVM_DIR/nvm.sh"
+  
+  print_info "Installing stable node version..."
+  nvm install stable
+else
+  print_error "Could not load nvm from $NVM_DIR/nvm.sh"
+fi
