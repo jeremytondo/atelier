b55a5435aeded0af69e43a6ded87082d0cbd7351
Jeremy Margaritondo <jmargari@gmail.com>
2025-12-15 23:04:58 -0600

    Don't run shpool tests after compile

--- install/additional-tools/shpool.sh
+++ install/additional-tools/shpool.sh
@@ -8,12 +8,12 @@ source "$SCRIPT_DIR/../common.sh"
 print_step "Installing shpool..."
 
 if check_command yay; then
-  yay -S --noconfirm --needed shpool
+  yay -S --noconfirm --needed --nocheck shpool
 
   print_info "Configuring systemd services for shpool..."
   systemctl --user enable shpool
   systemctl --user start shpool
-  
+
   # Enable linger for the current user so services run without an active session
   sudo loginctl enable-linger "$USER"
 else
