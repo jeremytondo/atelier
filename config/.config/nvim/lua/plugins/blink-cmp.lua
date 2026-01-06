c4e3e6a8311c3c6e3f0b91a651391c7f929d50ef
jeremytondo <jmargari@gmail.com>
2026-01-05 21:39:42 -0600

    Don't autocomplete in markdown files

---
+++ config/.config/nvim/lua/plugins/blink-cmp.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "saghen/blink.cmp",
+    opts = {
+      enabled = function()
+        return not vim.tbl_contains({ "markdown" }, vim.bo.filetype)
+          and vim.bo.buftype ~= "prompt"
+          and vim.b.completion ~= false
+      end,
+    },
+  },
+}
