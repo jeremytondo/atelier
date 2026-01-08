741ea677502b504b5225e6d577ad721f82a19773
jeremytondo <jmargari@gmail.com>
2026-01-08 15:21:25 -0600

    Re-enabled autocomplete for markdown.

--- config/.config/nvim/lua/plugins/blink-cmp.lua
+++ config/.config/nvim/lua/plugins/blink-cmp.lua
@@ -1,12 +1,6 @@
 return {
   {
     "saghen/blink.cmp",
-    opts = {
-      enabled = function()
-        return not vim.tbl_contains({ "markdown" }, vim.bo.filetype)
-          and vim.bo.buftype ~= "prompt"
-          and vim.b.completion ~= false
-      end,
-    },
+    opts = {},
   },
 }
