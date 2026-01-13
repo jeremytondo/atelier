87a5a6fb48f89606485bdb14a2a433a0186a46ae
jeremytondo <jmargari@gmail.com>
2026-01-13 06:28:07 -0600

    Updated markdown file rendering

--- config/.config/nvim/lua/config/autocmds.lua
+++ config/.config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,20 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    -- Only affects Markdown files
+    vim.opt_local.breakindent = true
+    vim.opt_local.linebreak = true
+
+    -- The 'shift' actually respects the starting indentation.
+    -- If a line has 0 indent (no bullet), it shifts 0.
+    -- If it's a bullet, it adds the extra 2 to align under the text.
+    vim.opt_local.breakindentopt = "list:2"
+
+    -- This tells Neovim to treat bullets as list markers for wrapping
+    vim.opt_local.formatoptions:append("n")
+  end,
+})
---
+++ config/.config/nvim/lua/plugins/zen.lua
@@ -0,0 +1,16 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    zen = {
+      toggles = {
+        dim = false,
+      },
+      show = {
+        statusline = true,
+      },
+      win = {
+        width = 80,
+      },
+    },
+  },
+}
