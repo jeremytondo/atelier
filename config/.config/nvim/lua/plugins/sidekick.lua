27b751b32b185fd4de7b0630fc49d3127d410f40
Jeremy Margaritondo <jmargari@gmail.com>
2026-01-22 21:20:17 -0600

    Sidekick update

--- config/.config/nvim/lua/plugins/sidekick.lua
+++ config/.config/nvim/lua/plugins/sidekick.lua
@@ -3,6 +3,23 @@ return {
     "folke/sidekick.nvim",
     opts = {
       nes = { enabled = false }, -- This disables Copilot NES features
+      copilot = {
+        status = { enabled = false },
+      },
+      cli = {
+        win = {
+          split = {
+            width = 95, -- Increase width from default 80 to 100 columns
+          },
+        },
+      },
     },
+    keys = {
+      {
+        "<leader>ag",
+        function() require("sidekick.cli").toggle({ name = "gemini", focus = true }) end,
+        desc = "Sidekick Toggle Gemini",
+      },
+    }
   },
 }
